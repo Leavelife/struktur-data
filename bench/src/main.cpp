2909695d9c20dcf7c5723d4c318f69cc952076e0
Leavelife <nurusyah369@gmail.com>
2025-01-26 09:16:19 +0700

    add latihan 1

--- bench/src/main.cpp
+++ bench/src/main.cpp
@@ -2,27 +2,9 @@
 #include <string>
 using namespace std;
 
-class Mahasiswa {
-    private:
-        string nama, NIM;
-    public:
-        Mahasiswa(const string nama, const string NIM) {
-            this->nama = nama;
-            this->NIM = NIM;
-        }
-        void read() {
-            cout << "Nama Mahasiswa: " << Mahasiswa::nama << "\nNIM: " << this->NIM << endl << endl;
-        }
-};
-
 int main() {
-    string inputNama;
-    string inputNIM;
-    cout << "Masukkan data mahasiswa" << endl;
-    cout << "Nama: "; cin >> inputNama;
-    cout << "NIM: "; cin >> inputNIM;
-
-    Mahasiswa mhs1(inputNama, inputNIM);
-    mhs1.read();
-
+    cout << endl;
+    cout << "welcome in cpp world" << endl;
+    cout << "welcome in cpp world" << endl;
+    cout << endl;
 }
--- modul-1/latihan.cpp
+++ modul-1/latihan.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+#define MAX 10
+
+int fibo[MAX];
+
+int main() {
+    int i;
+    fibo[1] = 1;
+    fibo[2] = 1;
+    for (int i = 3; i <= MAX; i++) {
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+    for (int i = 1; i <= MAX; i++) {
+        cout << i << ". " << fibo[i] << endl;
+    }
+}
